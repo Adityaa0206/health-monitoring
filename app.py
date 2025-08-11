02a4db3b6bb201e7a2d6a7b6b3b526dd45f3e551
Adityaa0206 <adiharsh0206@gmail.com>
2025-08-12 02:25:19 +0530

    Fix main guard syntax error

--- app.py
+++ app.py
@@ -134,4 +134,7 @@ def get_warning_logs():
 
     return jsonify(logs)
 
-if __na
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
